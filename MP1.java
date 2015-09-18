8ae5549eee6b986e18d56b08961144be80de11f5
Yamunathangam <dyamunacse@gmail.com>
2015-09-18 20:35:10 +0530

    java -jar MP1.jar 12940541

--- MP1.java
+++ MP1.java
@@ -51,7 +51,73 @@ public class MP1 {
 
     public String[] process() throws Exception {
         String[] ret = new String[20];
-       
+      
+        
+        String s[][]=new String[30][200];
+        String st ="";
+        File filepath=new File(inputFileName);
+        FileInputStream fin=new FileInputStream(filepath);
+        //FileInputStream fin1=new FileInputStream(filepath);
+        int i,j=0,k=0,count=1;
+        
+        String st1="",st2="";
+        List l=new ArrayList();
+        while((i=fin.read())!=-1)
+        {
+                
+                st=st+(char)i;
+                char c =(char)i;
+                if(c=='\n')
+                {
+                    StringTokenizer str=new StringTokenizer(st,delimiters);
+                    while(str.hasMoreTokens())
+                    {
+                      StringBuilder sb=new StringBuilder(str.nextToken().toLowerCase().trim());
+                 //if(Character.isWhitespace(sb.charAt(0)))
+                  //System.out.print("hello");
+                     String stt=sb.toString();
+                    l.add(stt);
+                    count++;
+                    }
+                    
+                    st="";
+                }
+        }   
+        
+        HashSet m=new HashSet();
+        Collections.sort(l);
+         System.out.println(l);
+         j=1;
+         for(int f=0;f<count-1;f++)
+         {
+             String st5=l.get(f).toString();
+               for(int ft=0;ft<count-1;ft++)
+               {
+           String st6=l.get(ft).toString();
+             if(st5.equalsIgnoreCase(st6))
+             {
+                 ++k;
+               }
+           
+               }  
+               
+             //System.out.println(l.get(f).toString()+k);
+               m.add((st5+k));
+                 k=0;
+                 
+                // j++;
+                 
+                      }
+             //System.out.println(t.toString()+count);ject
+         
+    //Collection stmap = m.values()
+         
+    Iterator itr =m.iterator();
+    while(itr.hasNext())
+      System.out.println(itr.next());
+            System.out.println("finished");
+        
+          
         //TODO
 
         return ret;
